05fbb052b37b7595bc88760d66df40a9a86bb2ac
zomath <duansongtoki@icloud.com>
2018-08-07 10:58:01 +0800

    daun

---
+++ codecademy/is_prime.py
@@ -0,0 +1,11 @@
+x = 3
+def is_prime(x):
+    if x < 2:
+        return False
+    else:
+        for n in range(2, x-1):
+            if x % n == 0:
+                return False
+        return True
+
+print(is_prime(x))
